38dada9367a2982c4929859856524bce3b2c5853
Vasu7777py <Subbannavarvasu@gmail.com>
2022-07-05 11:28:55 +0530

    Frontend complete

--- app/src/main/java/com/example/emi/MainActivity.java
+++ app/src/main/java/com/example/emi/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.emi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    double EMI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void CalculateEmi(View view) {
     }
 }
